c803a4960c9fc3f36b43aeaebb98c80ccafa07f4
John D <az0006t@protonmail.com>
2025-11-05 21:24:47 -0500

    Clean up unused function

--- compat/boost/asio/deadliner_timer.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <boost/asio.hpp>
-
-namespace boost { namespace asio {
-  // Provide old name for newer Boost (maps io_service -> io_context)
-  using deadline_timer = steady_timer; // map old name to new
-}}
